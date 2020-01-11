63e95a72e0c47618d40ea62b8325d5a9a12404ce
bpgg <97160186@qq.com>
2020-01-11 19:48:39 +0800

    构建项目、配置全局样式、element-ui

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-wrap">
+    <div class="nav-bar"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-wrap {
+  width: 100%;
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
